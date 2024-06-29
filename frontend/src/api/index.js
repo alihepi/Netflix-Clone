6516041472297788d05870b3b13cb6cb07db6379
Ali Mutlu <95170323+alihepi@users.noreply.github.com>
2024-06-29 15:12:32 +0300

    final-version

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import config from "../config";
 
 const apiClient = axios.create({
-  baseURL: config.API_BASE_URL,
+  baseURL: "https://netflix-clone-backend-alihappy.vercel.app",
 });
 
 export async function getSignedUrl({ key, content_type }) {
